584ecac3ebdd338e2a13cd6bcb6ab4fdf7daae3a
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-10 23:28:44 +0800

    complete all test

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,26 @@
-const parseData
-export { parseData };
+const parseData = function (input) {
+    const {data, column} = input;
+
+    // const person1 = {}
+    // person1[column[0].name]="Anna";
+    // person1[column[1].name]=16;
+    // person1[column[2].name]="Female";
+    // person1[column[3].name]="2004-12-01";
+    // const person2 = {}
+    // person2[column[0].name]="Kevin";
+    // person2[column[1].name]=22;
+    // person2[column[2].name]="Male";
+    // person2[column[3].name]="1998-01-21";
+
+    const arr = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const person = {}
+        for (let j = 0; j < data[i].length; j++) {
+            person[column[j].name]=data[i][j];
+        }
+        arr.push(person)
+    }
+    return arr;
+}
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = function (items,sections){
+    sections.sort((sec1,sec2)=>{
+        return sec2.index - sec1.index;
+    })
+    for (let i = 0; i < sections.length; i++) {
+        const content = sections[i].content;
+        const index = sections[i].index;
+        items.splice(index,0,content)
+    }
+    return items;
+}
 export { inject };
